b08313216b59b29a94323290c6e31370fe25ef95
MalcolmSmalls <smallsbeats@gmail.com>
2022-11-10 07:34:34 -0800

    Add extra-credit joke with no setup

--- src/App.js
+++ src/App.js
@@ -18,7 +18,9 @@ function App() {
     {setup: `Why do bees stay in the hive in the winter?`,
     punchline: `Swarm.`},
     {setup: `What's the best thing about Switzerland?`,
-    punchline: `I don't know, but the flag is a big plus!`}
+    punchline: `I don't know, but the flag is a big plus!`},
+    {setup: ``,
+    punchline: `Shut yo mouf!`}
 
   ]
 
@@ -30,7 +32,7 @@ function App() {
       <Joke setup="How did the hacker escape the police?" punchline="He just ransomware!"/>
       <Joke setup={jokeCollection[2].setup} punchline={jokeCollection[2].punchline}/>
       <Joke setup={pickedJoke.setup} punchline={pickedJoke.punchline}/>
-
+      <Joke setup={jokeCollection[5].setup} punchline={jokeCollection[5].punchline}/>
     </div>
   );
 }
--- src/Components/Joke.js
+++ src/Components/Joke.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react"
 
 function Joke({setup, punchline}){
-    return (
-        <div>
-            <p><strong>{setup}</strong></p>
-            <p>{punchline}</p>
-        </div>
-    )
-
+    console.log({setup})
+    if({setup} === ''){
+        return (
+            <div>
+                <p>{punchline}</p>
+            </div>
+        )
+    }else {
+        return (
+            <div>
+                <p><strong>{setup}</strong></p>
+                <p>{punchline}</p>
+            </div>
+        )
+    }
 }
 
 export default Joke
